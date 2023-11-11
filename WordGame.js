8142dc466e0ce0bbe38e9d278df72e0f3545a5c9
ChewingOnCode <109337208+ChewingOnCode@users.noreply.github.com>
2023-11-11 17:57:03 -0500

    Updated code by removing backticks and templating
    - Removed backticks and templating
    -fixed issues in spacing for output.

--- WordGame.js
+++ WordGame.js
@@ -1,8 +1,34 @@
 /** @format */
 
-function greet() {
+function tellStory() {
+    // Gather form data
     let boyName = document.getElementById('boyName');
-    let username = txtName.value;
+    let girlName = document.getElementById('girlName');
+    let geoLocation = document.getElementById('geoLocation');
+    let txtverb = document.getElementById('verb');
+    let txtcontainer = document.getElementById('container');
+    let txtliquid = document.getElementById('liquid');
+    let bodyPart = document.getElementById('bodyPart');
+    let txtingVerb = document.getElementById('ingVerb');
     let output = document.getElementById('output');
-    output.innerHTML = 'Hi ' + username + '!';
+
+    // Create Variables
+    let boy = boyName.value;
+    let girl = girlName.value;
+    let location = geoLocation.value;
+    let verb = txtverb.value;
+    let container = txtcontainer.value;
+    let liquid = txtliquid.value;
+    let part = bodyPart.value;
+    let ingverb = txtingVerb.value;
+
+    // Write the story
+    let story = boy + ' and ' + girl + ' went up the ';
+    story += location + ' to ' + verb + ' a ' + container;
+    story += ' of ' + liquid + '.';
+    story += boy + ' fell down and broke his ' + part;
+    story += ' and ' + girl + ' came ' + ingverb + ' after.';
+
+    // Copy story to ouput
+    output.innerHTML = story;
 }
