9d4181bfa38dbab9d836ea5455942b051090b5bd
ChewingOnCode <109337208+ChewingOnCode@users.noreply.github.com>
2023-11-11 17:24:28 -0500

    Added JS and CSS Files
    This will update the files to be separated instead of inline styling

---
+++ WordGame.js
@@ -0,0 +1,8 @@
+/** @format */
+
+function greet() {
+    let boyName = document.getElementById('boyName');
+    let username = txtName.value;
+    let output = document.getElementById('output');
+    output.innerHTML = 'Hi ' + username + '!';
+}
